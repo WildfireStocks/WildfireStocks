2dc1429de16e89ea58bf86c393b0acafc61c6335
JHanhivaara <77466940+JHanhivaara@users.noreply.github.com>
2021-01-16 20:45:38 +0200

    material ui / SideNav

---
+++ src/components/SideNav/SideNav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SideNav = (props) => {
+   return (
+      <div className="SideNav">
+         <a className="Home"></a>
+         <a className="About"></a>
+         <a className="Stock Search"></a>
+         <a className="Global"></a>
+         <a className="Domestic"></a>
+         <a className="Your Profile"></a>
+      </div>
+   )
+}
+export default SideNav;
