30c95e0e3532b2a81d3568fe8f0effe8757421b9
jaakop <jaakko@kronogra.fi>
2021-01-18 19:43:55 +0200

    ported to vanilla reactjs

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+import './App.css'
+import Header from './components/Header.js'
+
+
+// markup
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <Header/>
+    </main>
+  )
+}
+
+export default IndexPage
