b96f79af8b4d6522417271e5fca366f16c69ccf4
Jaakko <jaakko@kronogra.fi>
2021-01-23 19:49:23 +0200

    made the stuff easy to read

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { makeStyles } from '@material-ui/core/styles';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import Button from '@material-ui/core/Button';
@@ -7,6 +8,7 @@ import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+
 import MenuIcon from '@material-ui/icons/Menu';
 import HomeIcon from '@material-ui/icons/Home';
 import ChartIcon from '@material-ui/icons/ShowChart';
