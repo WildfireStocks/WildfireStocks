2a1eeb3fdabb25dbbd89bd8bb9426f87063d5604
Jaakko <jaakko@kronogra.fi>
2021-01-24 18:49:41 +0200

    added page

---
+++ src/pages/stocksPage.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+export default class StockView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+}
